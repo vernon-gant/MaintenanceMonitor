91603943b8f0c8848ce13ba71032984c96752554
Aleksandr Zakharov <aleksza.4119@gmail.com>
2022-05-10 18:20:33 +0200

    Initial Commit.

---
+++ src/test/java/maintenance_monitor/MaintenanceMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package maintenance_monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
